465bb944928087dfdbb171ec046be503a274c312
Brian Chan <brianchan@utexas.edu>
2016-11-15 09:02:31 -0800

    updating grunt automation

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,34 +3,34 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
-    // jade: {
-    //   compile: {
-    //     options: {
-    //       data: {
-    //         debug: false
-    //       }
-    //     },
-    //     files: {
-    //       'public/views/home.html' : 'public/views/home.jade'
-    //     }
-    //   }
-    // },
-
     // configure nodemon
     nodemon: {
       dev: {
         script: 'server.js'
       }
+    },
+
+    // configure modernizr
+    modernizr: {
+      dist: {
+        "crawl": false,
+        "customTests": [],
+        "dest": "./bin/modernizr.js",
+        "tests": [
+          "touchevents"
+        ],
+        "options": [
+          "setClasses"
+        ],
+        "uglify": true
+      }
     }
 
   });
 
-  // load nodemon
   grunt.loadNpmTasks('grunt-nodemon');
-  // grunt.loadNpmTasks('grunt-contrib-jade')
+  grunt.loadNpmTasks('grunt-modernizr')
 
-  // register the nodemon task when we run grunt
-  // grunt.registerTask('default', ['nodemon', 'jade']); 
-  grunt.registerTask('default', ['nodemon']); 
+  grunt.registerTask('default', ['modernizr:dist', 'nodemon']); 
 
 };
--- public/src/js/appRoutes.js
+++ public/src/js/appRoutes.js
@@ -3,47 +3,47 @@
 
     $routeProvider
         .when('/', {
-            templateUrl: 'src/views/partials/home.html'
+            templateUrl: 'public/src/views/partials/home.html'
         })
 
         .when('/resume', {
-            templateUrl: 'src/views/partials/resume.html'
+            templateUrl: 'public/src/views/partials/resume.html'
         })
 
         .when('/media', {
-            templateUrl: 'src/views/partials/media.html',
+            templateUrl: 'public/src/views/partials/media.html',
             controller: 'MapController'
         })
 
         .when('/aboutme', {
-            templateUrl: 'src/views/partials/aboutme.html',
+            templateUrl: 'public/src/views/partials/aboutme.html',
         })
 
         .when('/blog', {
-            templateUrl: 'src/views/partials/blog.html',
+            templateUrl: 'public/src/views/partials/blog.html',
             controller: 'BlogController'
         })
 
         .when('/blog/:url', {
-            templateUrl: 'src/views/partials/blog-detail.html',
+            templateUrl: 'public/src/views/partials/blog-detail.html',
             controller: 'BlogController'
         })
         .when('/twitter', {
-            templateUrl: 'src/views/partials/twitter.html',
+            templateUrl: 'public/src/views/partials/twitter.html',
             controller: 'TwitterController'
         })
         .when('/admin/:url', {
-            templateUrl: 'src/views/partials/admin-blog-detail.html',
+            templateUrl: 'public/src/views/partials/admin-blog-detail.html',
             controller: 'BlogController'
         })
 
         .when('/admin', {
-            templateUrl: 'src/views/partials/admin.html',
+            templateUrl: 'public/src/views/partials/admin.html',
             controller: 'BlogController'
         })
 
         .when('/login', {
-            templateUrl: 'src/views/partials/login.ejs'
+            templateUrl: 'public/src/views/partials/login.ejs'
         })
 
     $locationProvider.html5Mode(true);
