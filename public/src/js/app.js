91ffd0b003e4e5b86cf5943aed8c2cef54bef56a
Brian Chan <brianchan@utexas.edu>
2017-01-07 12:26:23 -0800

    adding angular-google-analytics

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,8 +1,12 @@
-angular.module('myApp', ['ngRoute', 'appRoutes', 'ngSanitize', 'ui.bootstrap', 'nemLogging', 'uiGmapgoogle-maps', 'MainDir', 'BlogCtrl', 'InstagramCtrl', 'MapCtrl', 'TwitterCtrl', 'BlogService', 'AdminCtrl'])
+angular.module('myApp', ['ngRoute', 'appRoutes', 'ngSanitize', 'ui.bootstrap', 'nemLogging', 'uiGmapgoogle-maps', 'angular-google-analytics', 'MainDir', 'BlogCtrl', 'InstagramCtrl', 'MapCtrl', 'TwitterCtrl', 'BlogService', 'AdminCtrl'])
 .config(function(uiGmapGoogleMapApiProvider) {
     uiGmapGoogleMapApiProvider.configure({
         key: 'AIzaSyA8l8STCfBE-JKWfz3BFMRWMtiuhroPDfY',
         v: '3.20',
         libraries: 'weather,geometry,visualization,places'
     });
-});
+})
+.config(['AnalyticsProvider', function (AnalyticsProvider) {
+
+   AnalyticsProvider.setAccount('UA-89957529-1');
+}]).run(['Analytics', function(Analytics) { }]);
